cfb6acb0e9eae9ae7a85008cd3a5310ed2028a42
crom <kbofc@protonmail.com>
2023-02-07 13:12:49 +0800

    added cors 5 last 3

--- server.js
+++ server.js
@@ -92,4 +92,4 @@ async function fetchData(options, url) {
   });
 }
 
-app.listen(5000, () => {console.log("server started on port 5000")});
+app.listen(5000, () => {console.log("server started on port 80")});
