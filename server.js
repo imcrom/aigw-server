44071fae1c3ee4d0095ba006eb38eabf6ca4449a
crom <kbofc@protonmail.com>
2023-02-07 12:07:44 +0800

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+var cors = require('cors')
 const app = express();
 
-
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 
 app.use(express.json());
@@ -12,7 +16,7 @@ app.get("/api", (req, res) => {
     res.json({"message": "Hello from server!"});
 });
 
-app.post("/api", async (req, res) => {
+app.post("/api", cors(corsOptions), async (req, res) => {
 
 
     const data = req.body;
